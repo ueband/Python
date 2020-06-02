02d095f6c931601465caddfcf95c7e45305d60eb
ueband <ueband@gmail.com>
2020-06-02 15:47:48 +0300

    lesson-2

---
+++ hw2/6.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+goods = []
+features = {'name': '', 'price': '', 'quantity': '', 'unit': ''}
+analytics = {'name': [], 'price': [], 'quantity': [], 'unit': []}
+num = 0
+feature_ = None
+control = None
+while True:
+    control = input("For quit press 'Q', for continue press 'Enter', for analytics press 'A'").upper()
+    if control == 'Q':
+        break
+    num += 1
+
+    if control == 'A':
+        print(f'\n Current analytics \n {"-" * 30}')
+        for key, value in analytics.items():
+            print(f'{key[:25]:>30}: {value}')
+            print("-" * 30)
+    for f in features.keys():
+        feature_ = input(f'Input feature "{f}"')
+        features[f] = int(feature_) if (f == 'price' or f == 'quantity') else feature_
+        analytics[f].append(features[f])
+        print(features)
+    goods.append((num, features))
+    print(goods)
+
+
+
+
+
